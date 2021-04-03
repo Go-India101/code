19b51054b093919066326abf7fcb54ddab34a2ae
Go India & PlayDude <69028055+Go-India101@users.noreply.github.com>
2021-04-03 13:13:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,29 +3,35 @@ function change1(){
     document.getElementById("css").style.display = "none";
     document.getElementById("js").style.display = "none";
     document.getElementById("code").style.display = "none";
-    document.getElementById("copy1").style.display = "none";
-    document.getElementById("copy2").style.display = "none";
-    document.getElementById("copy3").style.display = "none";
-    document.getElementById("br1").style.display = "block";
-    document.getElementById("br2").style.display = "block";
-    document.getElementById("br3").style.display = "block";
-    document.getElementById("br1").style.display = "none";
-    document.getElementById("br2").style.display = "none";
-    document.getElementById("br3").style.display = "none";
-    document.getElementById("br").style.display = "none";
+    document.getElementById("copy11").style.display = "block";
+    document.getElementById("copy12").style.display = "none";
+    document.getElementById("copy13").style.display = "none";
+    document.getElementById("buttonhtml").style.color = "none";
+    document.getElementById("buttonhtml").style.backgroundColor = "#003f63";
+    document.getElementById("buttonhtml").style.color = "#40ff00";
+    document.getElementById("buttoncss").style.backgroundColor = "#002a42";
+    document.getElementById("buttoncss").style.color = "white";
+    document.getElementById("buttonjs").style.backgroundColor = "#002a42";
+    document.getElementById("buttonjs").style.color = "white";
+    document.getElementById("buttoncode").style.backgroundColor = "#002a42";
+    document.getElementById("buttoncode").style.color = "white";
     }
 function change2(){
     document.getElementById("js").style.display = "none";
     document.getElementById("css").style.display = "block";
     document.getElementById("html").style.display = "none";
     document.getElementById("code").style.display = "none";
-    document.getElementById("copy1").style.display = "none";
-    document.getElementById("copy2").style.display = "none";
-    document.getElementById("copy3").style.display = "none";
-    document.getElementById("br1").style.display = "none";
-    document.getElementById("br2").style.display = "none";
-    document.getElementById("br3").style.display = "none";
-    document.getElementById("br").style.display = "none";
+    document.getElementById("copy11").style.display = "none";
+    document.getElementById("copy12").style.display = "block";
+    document.getElementById("copy13").style.display = "none";
+    document.getElementById("buttonhtml").style.backgroundColor = "#002a42";
+    document.getElementById("buttonhtml").style.color = "white";
+    document.getElementById("buttoncss").style.backgroundColor = "#003f63";
+    document.getElementById("buttoncss").style.color = "#40ff00";
+    document.getElementById("buttonjs").style.backgroundColor = "#002a42";
+    document.getElementById("buttonjs").style.color = "white";
+    document.getElementById("buttoncode").style.backgroundColor = "#002a42";
+    document.getElementById("buttoncode").style.color = "white";
     
 }
 function loader(){
@@ -36,55 +42,51 @@ function change3() {
     document.getElementById("css").style.display = "none";
     document.getElementById("html").style.display = "none";
     document.getElementById("code").style.display = "none";
-    document.getElementById("copy1").style.display = "none";
-    document.getElementById("copy2").style.display = "none";
-    document.getElementById("copy3").style.display = "none";
-    document.getElementById("br").style.display = "none";
-    document.getElementById("br1").style.display = "none";
-    document.getElementById("br2").style.display = "none";
-    document.getElementById("br3").style.display = "none";
+    document.getElementById("copy11").style.display = "none";
+    document.getElementById("copy12").style.display = "none";
+    document.getElementById("copy13").style.display = "block";
+    document.getElementById("buttonhtml").style.backgroundColor = "#002a42";
+    document.getElementById("buttonhtml").style.color = "white";
+    document.getElementById("buttoncss").style.backgroundColor = "#002a42";
+    document.getElementById("buttoncss").style.color = "white";
+    document.getElementById("buttonjs").style.backgroundColor = "#003f63";
+    document.getElementById("buttonjs").style.color = "#40ff00";
+    document.getElementById("buttoncode").style.backgroundColor = "#002a42";
+    document.getElementById("buttoncode").style.color = "white";
     }
 function change4() {
     document.getElementById("js").style.display = "none";
     document.getElementById("css").style.display = "none";
     document.getElementById("html").style.display = "none";
     document.getElementById("code").style.display = "block";
-    document.getElementById("copy1").style.display = "none";
-    document.getElementById("copy2").style.display = "none";
-    document.getElementById("copy3").style.display = "none";
-    document.getElementById("br").style.display = "none";
-    document.getElementById("br1").style.display = "none";
-    document.getElementById("br2").style.display = "none";
-    document.getElementById("br3").style.display = "none";
+    document.getElementById("copy11").style.display = "none";
+    document.getElementById("copy12").style.display = "none";
+    document.getElementById("copy13").style.display = "none";
+    document.getElementById("buttonhtml").style.backgroundColor = "#002a42";
+    document.getElementById("buttonhtml").style.color = "white";
+    document.getElementById("buttoncss").style.backgroundColor = "#002a42";
+    document.getElementById("buttoncss").style.color = "white";
+    document.getElementById("buttonjs").style.backgroundColor = "#002a42";
+    document.getElementById("buttonjs").style.color = "white";
+    document.getElementById("buttoncode").style.backgroundColor = "#003f63";
+    document.getElementById("buttoncode").style.color = "#40ff00";
     }
-    function change5() {
-        document.getElementById("js").style.display = "none";
-        document.getElementById("css").style.display = "none";
-        document.getElementById("html").style.display = "none";
-        document.getElementById("code").style.display = "none";
-        document.getElementById("copy1").style.display = "block";
-        document.getElementById("copy2").style.display = "block";
-        document.getElementById("copy3").style.display = "block";
-        document.getElementById("br").style.display = "block";
-        document.getElementById("br1").style.display = "block";
-        document.getElementById("br2").style.display = "block";
-        document.getElementById("br3").style.display = "block";
-        }
-    function copy1() {
-  var copyText = document.getElementById("html");
-  copyText.select();
-  copyText.setSelectionRange(0, 99999);
-  document.execCommand("copy");
-    }
-    function copy2() {
-        var copyText = document.getElementById("css");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
-          }
-          function copy3() {
-            var copyText = document.getElementById("js");
+        function copy1() {
+            var copyText = document.getElementById("html");
             copyText.select();
-            copyText.setSelectionRange(0, 99999);
+            copyText.setSelectionRange(0, 9999999999999999);
             document.execCommand("copy");
               }
+              function copy2() {
+                  var copyText = document.getElementById("css");
+                  copyText.select();
+                  copyText.setSelectionRange(0, 9999999999999999);
+                  document.execCommand("copy");
+                    }
+                    function copy3() {
+                      var copyText = document.getElementById("js");
+                      copyText.select();
+                      copyText.setSelectionRange(0, 9999999999999999);
+                      document.execCommand("copy");
+                        }
+
